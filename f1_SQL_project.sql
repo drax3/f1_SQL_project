3d835879090ce0d162c6b72524aabe33ee1d4851
Tushar <tsompura0006@gmail.com>
2022-01-19 14:30:51 +0530

    add sql project file

--- f1_SQL_project.sql
+++ f1_SQL_project.sql
@@ -1,12 +1,20 @@
--- driver - qualifying - constructor - venues 
+-- There are 3 events in general in F1 race weekend 1.) practice 2.) qualifying 3.) race
+-- There were few races with new suggested format taken from F2 weekend. That involves a sprint race. 
+-- This dataset consists of race and qualifying data. Other useful and vital information about Constructor and driver along with venue is also present.  
+-- The queries are divided into following categories-
+-- 1.)driver 2.)qualifying 3.)venues 4.)constructor 
+-- driver is a person who drives one of the car of constructor. Over the period of time the maximum points raised from 10 to 25 and allocationpf points vary no- linearly and with new rule that gives point to fastest lap holder in the race.
+-- champion driver is the one with maximum points at the end of the season. 
+-- qualifying is the event where grid for the race is decided. Format has changed throughout the history with current envolves three session q1(20) q2(15) q3(10) with elimation of 5 drivers in each session 
+-- venue is the host circuit for the race weekend. they are on contractual basis for a period of time in f1 calender.
+-- constructor are the teams which participates in racing. each constructor have 2 car and drivers separately. constructor champioship is decided by sum of two driver points.
 
--- driver
 
--- most championship winner
+-- 1.) driver 
+-- total championship won by drivers
 SELECT 
-	number,
-    code,
     forename,
+    surname,
     count(*) AS driver_championships
 FROM (
 	SELECT *
@@ -18,7 +26,8 @@ ORDER BY driver_championships DESC;
 
 -- most race wins in career 
 SELECT  
-	d.forename, 
+	d.forename,
+    d.surname,
     count(*) AS wins
 FROM results r
 JOIN drivers d
@@ -28,21 +37,21 @@ GROUP BY r.driverId
 ORDER BY wins DESC;
 
 
--- most race wins in a season top 15
+-- most race wins by a driver in a season top 15
 -- improvement 1. add constructors name
 SELECT 
 	rs.year,
-    r.driverId, 
-    r.constructorId,
-    r.number,
-    d.driverRef,
-    d.forename, 
+    d.forename,
+    d.surname,
+    c.name,
     count(*) AS wins
 FROM results r
 JOIN races rs
 	USING(raceId)
 JOIN drivers d
 	USING(driverId)
+join constructors c
+	using(constructorId)
 WHERE position = 1
 GROUP BY d.driverId, rs.year
 ORDER BY wins DESC
@@ -50,10 +59,8 @@ LIMIT 15;
 
 -- most wins of non champion driver
 SELECT 
-    r.constructorId,
-    r.number,
-    d.driverRef,
-    d.forename, 
+    d.forename,
+    d.surname,
     count(*) AS wins
 FROM results r
 JOIN races rs
@@ -66,11 +73,11 @@ GROUP BY d.forename
 ORDER BY wins DESC
 LIMIT 10;
 
--- most career points
+-- top total career points of driver
 SELECT 
 	number, 
-    code, 
 	forename, 
+    surname,
     sum(points) AS career_points
 FROM driver_championship_standings
 GROUP BY forename
@@ -79,33 +86,16 @@ ORDER BY career_points DESC;
 
 -- drivers without career points
 -- add no of races they participated
-SELECT
-	number, 
-    code, 
-	forename, 
+SELECT 
+	forename,
+    surname,
     sum(points) AS career_points
 FROM driver_championship_standings
 GROUP BY forename
 HAVING career_points = 0
 ORDER BY career_points DESC;
 
--- drivers with most out of points finish
-
--- drivers without points
-SELECT *
-FROM drivers 
-WHERE forename NOT IN (
-SELECT forename 
-FROM (
-SELECT number, code, forename, sum(points) as career_points
-FROM driver_championship_standings
-GROUP BY forename
-HAVING career_points <> 0
-ORDER BY career_points DESC
-) careen_points_table
-);
-
--- most race participatioin
+-- driver with 200 plus race participatioin
 SELECT  
 	-- r.driverId,
     d.forename,
@@ -116,29 +106,31 @@ JOIN drivers d
 	USING(driverId)
 -- where r.grid <> 0
 GROUP BY driverId
+HAVING races >= 200
 ORDER BY races DESC;
 
 -- pole and win in the same race
 SELECT
 	d.forename,
     d.surname,
-    count(*) start_win 
+    count(*) start_finish_win 
 FROM results rslt
 JOIN drivers d
 	USING(driverId)
 WHERE rslt.grid = 1 AND rslt.position = 1
 GROUP BY rslt.driverId
-ORDER BY start_win DESC
+ORDER BY start_finish_win DESC
 LIMIT 10;
 
--- not finished due to particular reasons
+-- drivers with MOST unusal not-finishes
+SELECT *
+from (
 SELECT 
 	d.forename,
     d.surname,
     rslt.driverId, 
-    rslt.statusId,
     s.status,
-    count(*) AS not_count
+    count(*) AS count
 FROM results rslt
 JOIN drivers d
 	USING(driverId)
@@ -146,7 +138,10 @@ JOIN status s
 	USING(statusId)
 WHERE rslt.statusId IN (139, 100,72,60,20)
 GROUP BY rslt.driverId, rslt.statusId
-ORDER BY s.status, not_count DESC;
+ORDER BY s.status, count DESC
+) as tab
+GROUP BY status
+ORDER BY count DESC;
 
 
 -- qualifying ------------------------------------------------------------------------------------
@@ -163,17 +158,20 @@ GROUP BY rslt.driverId
 ORDER BY starts DESC
 LIMIT 10;
 
--- pole sitter in a season
+-- most pole sitter in a season
 SELECT 
 	rs.year,
 	d.forename,
     d.surname,
+    c.name,
     count(*) starts 
 FROM results rslt 
 JOIN drivers d
 	USING(driverId)
 JOIN races rs
 	USING(raceId)
+JOIN constructors c
+	USING(constructorId)
 WHERE grid = 1
 GROUP BY rslt.driverId, rs.year
 ORDER BY starts DESC
@@ -194,12 +192,11 @@ LIMIT 10;
 
 
 -- venue -------------------------------------------------------
--- most wins at venue 
+-- most wins at a venue 
 SELECT 
 	rslt.driverId,
     d.forename,
     d.surname,
-    rs.circuitId,
     rs.name,
     count(*) AS count
 FROM results rslt
@@ -235,7 +232,7 @@ GROUP BY year
 GROUP BY name
 ORDER BY const_champ DESC;
 
--- most wins of constructor in a year
+-- constructor with most wins in a year
 SELECT * 
 FROM constructor_wins
 GROUP BY year
@@ -258,7 +255,7 @@ FROM final_constructor_standings
 GROUP BY year
 ORDER BY points DESC;
 
--- maximum points scored by each constructore
+-- maximum points scored by each constructor in there career in f1
 SELECT * 
 FROM 
 (
@@ -287,7 +284,12 @@ LIMIT 10;
 
 
 -- when champion driver does not belong to champion constructor
-SELECT *
+SELECT 
+	dri.year, 
+    dri.forename, 
+    dri.surname, 
+    dri.name as driver_champ_team, 
+    const.name as constructor_champ
 FROM
 (SELECT * FROM driver_championship_standings GROUP BY year) AS dri
 JOIN 
@@ -297,7 +299,7 @@ WHERE dri.constructorId <> const.constructorId;
 
 
 -- ---------------------------------------------------------------------------------
--- this part contains additional helpful views and queries for the main queries
+-- this part contains additional helpful views for the main queries
 -- ----------------------------------------------------------------------------------
 
 -- all champion names
@@ -325,20 +327,22 @@ select
     d.number, 
     d.code, 
     d.forename,
+    d.surname,
     SUM(rlt.points) as points,
-    rlt.constructorId
+    rlt.constructorId,
+    c.name
 from results rlt
 join drivers d
 	using(driverId)
 join races rs
 	using(raceId)
--- where rs.year = 2021
+join constructors c
+	using(constructorId)
 group by driverId, rs.year
 order by rs.year desc, points desc;
 
 
 -- constructor points after every 
-select * from final_constructor_standings;
 create or replace view final_constructor_standings as
 select 
 	rs.year, 
@@ -369,11 +373,3 @@ join constructors c
 where rslt.position = 1
 group by rs.year, rslt.constructorId
 order by rs.year desc, wins desc;
-
-
--- last round of every race
-select raceId, year, max(round ) as last_round
-from races
-group by year 
-order by year;
-
